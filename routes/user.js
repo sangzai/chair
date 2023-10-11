d84ea627230a3c1bc426e953ba33be096050712a
CHS <wa159@naver.com>
2023-10-11 11:43:06 +0900

    새로운 데이터베이스와 회원가입 연동

--- routes/user.js
+++ routes/user.js
@@ -5,15 +5,16 @@ const conn = require("../config/database");
 // 회원가입 기능
 router.post('/handleJoin', (req, res) => {
   console.log('handleJoin', req.body);
-  let { userId, userPw, userName, gender, em, bloodType, birth, cm, kg } = req.body;
-  let email = em + '@' + bloodType
-  let height = parseFloat(cm)
-  let weight = parseFloat(kg)
+  let { userId, userPw, userName, userGender, em, bloodType, userBirthdate, cm, kg } = req.body;
+  let userEmail = em + '@' + bloodType
+  let userHeight = parseFloat(cm)
+  let userWeight = parseFloat(kg)
+  const createdAt = new Date();
   // 1) 내가 사용할 sql 쿼리문 작성
-  let sql = "insert into USERS values(?,?,?,?,?,?,?,?)";
+  let sql = "insert into users values(?,?,?,?,?,?,?,?,?)";
   // 2) DB 연동
   // conn.query(sql구문, sql구문 속 가변데이터(선택),연동 됐을 때 실행될 콜백함수)
-  conn.query(sql, [userId, userPw, userName, email, gender, birth, weight, height], (err, rows) => {
+  conn.query(sql, [userId, userPw, userName, userEmail, userGender, userBirthdate, userWeight, userHeight,createdAt], (err, rows) => {
     if (rows) {
       console.log('rows : ', rows)
       res.send('<script>alert("가입을 축하합니다!");location.href="/"</script>')
