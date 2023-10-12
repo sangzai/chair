20895ff2ac72db16a39e91372da4e8643580d0d7
sangzai <dwarf512396@gmail.com>
2023-10-13 00:35:11 +0900

    수정

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static(__dirname + "/public"));
 // app.use(express.static(__dirname + "/react-chair/build"));
-
+-
 app.use(
   session({
     httpOnly: true,
