53938e58893b5ba9fc42e0b7cd60c772cd12b255
CHS <wa159@naver.com>
2023-10-16 11:05:06 +0900

    실시간데이터전송

--- routes/statistics.js
+++ routes/statistics.js
@@ -40,7 +40,6 @@
 
 // module.exports = router;
 
-// 10-14 데이터 연결 시험
 const express = require("express");
 const router = express.Router();
 const conn = require("../config/database");
@@ -50,9 +49,9 @@ const bcrypt = require('bcrypt');
 router.post('/userstatistics', (req, res) => {
     const { selDate } = req.body;
     const pattern = '%' + selDate + '%';
-    const sql = 'SELECT COUNT(*) AS count FROM sensordata WHERE createdAt LIKE ?'; // COUNT(*)를 사용하여 레코드 수를 가져옴
+    const sql = 'SELECT COUNT(*) AS count FROM sensordata WHERE createdAt LIKE ? and userId = ?'; // COUNT(*)를 사용하여 레코드 수를 가져옴
     console.log(selDate)
-    conn.query(sql, [pattern], (err, rows) => {
+    conn.query(sql,[pattern,eq.session.user.userId],(err,rows)=>{
         if (!err) {
             const count = rows[0].count; // 레코드 수는 rows[0].count에 저장됨
             if (count > 0) {
@@ -67,9 +66,19 @@ router.post('/userstatistics', (req, res) => {
         }
     });
   });
+   
+// 실시간 연결
+router.get('/data', (req, res) => {
+    const userId = req.session.user.userId; // 원하는 userID로 설정
   
-  //user정보 bmi 변환 
-  
+    conn.query('SELECT * FROM sensordata WHERE userId = ? ORDER BY createdAt DESC LIMIT 1', [userId], (err, result) => {
+      if (err) {
+        console.error('데이터베이스 쿼리 오류:', err);
+        return res.status(500).json({ error: '데이터를 가져오는 중 오류가 발생했습니다.' });
+      }
+      res.json(result[0]);
+    });
+  });
 
   module.exports = router; 
 
